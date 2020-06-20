6ec7c03c52131956bad04aa29ded99444761348c
Jieun <jieunijeong9@anver.com>
2020-06-20 20:55:23 +0900

    Initial commit

---
+++ src/main/java/com/lover/cute/controller/HelloWorld.java
@@ -0,0 +1,17 @@
+package com.lover.cute.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HelloWorld {
+    @GetMapping("/loveme")
+    public String love(){
+        return "loveya";
+    }
+
+    @GetMapping("/hateyou")
+    public String hate(){
+        return "hateya";
+    }
+}
