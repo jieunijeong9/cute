4c5466fea637b08ad9c7c7675cb8da42f7d9a688
Jieun <jieunijeong9@anver.com>
2020-07-01 15:42:49 +0900

    add ajax

--- src/main/java/com/lover/cute/controller/ArticleController.java
+++ src/main/java/com/lover/cute/controller/ArticleController.java
@@ -1,10 +1,13 @@
 package com.lover.cute.controller;
 
 
+import com.lover.cute.dto.ArticleForm;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-
+import org.springframework.web.bind.annotation.PostMapping;
+@Slf4j
 @Controller
 public class ArticleController {
     @GetMapping("/articles")
@@ -12,4 +15,16 @@ public class ArticleController {
         model.addAttribute("msg", "안녕하세요, 반갑습니다!");
         return "articles/index";
     }
+
+    @GetMapping("/articles/new")
+    public String newArticle() {
+        return "articles/new";
+    }
+
+    @PostMapping("/articles") // Post 방식으로 "/articles" 요청이 들어오면, 아래 메소드 수행!
+    public String create(ArticleForm form) { // 폼 태그의 데이터가 ArticleForm 객체로 만들어 짐!
+        log.info(form.toString()); // ArticleForm 객체 정보를 확인!
+        return "redirect:/articles"; // 브라우저를 "/articles" url로 보냄!
+    }
 }
+
